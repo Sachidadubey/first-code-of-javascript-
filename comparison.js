2e5d163cde90bc3b76a649da15a3078d5b4dc99e
Sachidadubey <sachidanand2580@gmail.com>
2025-04-24 09:51:18 +0000

    comparison commit

---
+++ comparison.js
@@ -0,0 +1,21 @@
+// console.log(2==2);
+// console.log(2>=2);
+// console.log(2<=2);true
+// console.log(2!=2);false
+// console.log(2==2); true
+console.log("2">1);//true     just avoid doiing this
+console.log("02">1)//true
+// console.log(null>0);  js convert null value into the zero or nan
+// console.log(null==0);
+// console.log(null>=0);
+// console.log(undefined==0);
+// console.log(undefined>=0);
+// console.log(undefined<=0);false 
+// console.log(undefined < 0);
+
+
+// strict check ===
+console.log("0"==0);//true
+console.log("0"===0);//flase 
+
+
