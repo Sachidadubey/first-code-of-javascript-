4eb55251d8dfb12abd1657852370b69e3cbe9121
Sachidadubey <sachidanand2580@gmail.com>
2025-04-24 08:58:06 +0000

    DataType commit

---
+++ DataType.js
@@ -0,0 +1,14 @@
+"use strict"// treat all js code as newer verion
+// alert(3+3)  // we are using node js ,not browser
+console.log("dubey")
+console.log("ankush");
+
+const name="ankush";// string
+let age=18;// numeric 
+let t="true";// boolean
+//null => standalone value  (empty value)
+// undefined => till not defined
+//symbol => unique
+console.log(typeof undefined);// undefined as well
+console.log(typeof null);// object
+
