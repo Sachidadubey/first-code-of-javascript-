1f8f16ad145e6540c41f19a526cb4b61372f7931
Sachidadubey <sachidanand2580@gmail.com>
2025-04-24 09:16:36 +0000

    Type conversion commit

---
+++ DataTypeConversion.js
@@ -0,0 +1,23 @@
+// let  score=33;
+// console.log(typeof score); // number
+// let school="33abcd";
+// console.log(typeof school);// string
+// let intNumber= Number(school);
+// console.log(typeof intNumber);// number 
+// console.log(intNumber)//nan
+// af asssign null =>then ouput =0;
+//33=> 33
+//"33abc"=>nan
+//true=>1: false=0;
+let isLoggedIn=1;
+let booleanIsLoggedIn= Boolean(isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);//1 =>true
+
+//" "=>false
+
+// "string"=>true
+let someNumber=33;
+let stringNumber= String(someNumber);
+console.log(typeof stringNumber);// string
+console.log(stringNumber);//33
