b57c2c5caa031ce482e6f82a14f001ef36ef9c2c
Sachidadubey <sachidanand2580@gmail.com>
2025-04-25 06:37:29 +0000

    math commit

---
+++ NumberMath.js
@@ -0,0 +1,45 @@
+// let number=50;
+
+// const balance= new Number(100);
+// console.log(number);// 50 
+// console.log(balance);  //[number :100]
+
+// console.log(balance.toString().length);//in the string 
+//  console.log(balance.toFixed(3));//  3 digits after to decimal 
+//  const otherNmae=23.8966;
+
+//  console.log(otherNmae.toPrecision(4));// if we pass 3 then it would be round of using three digit 
+//  const hundreds =100000000;
+//  console.log(hundreds.toLocaleString('en-in'));// it separates commas according to indian standard 
+//  let no= Number.MIN_VALUE;
+//  console.log(no);// it will return  the minimum value 
+
+console.log(Math);// math is the object 
+console.log(Math.abs(-4));// 4
+
+console.log(Math.round(13.456));// make round figure  13 
+console.log(Math.ceil(12.567));// 13
+console.log(Math.min(2,4,5,6,7,8,9));// return minimum value 
+console.log(Math.max(2,3,4,45,6,));// return maximmum value 
+console.log(Math.random());// always give random value - btwn 0 & 1 
+console.log((Math.random()*10)+1);
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min);// always return the value btwn 10 and 20 
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+ 
+
+
